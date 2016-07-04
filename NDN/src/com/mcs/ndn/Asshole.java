c0878e4d23ae2f946069b1c4dd1fa85836512202
chmilk <jamesmaron87@gmail.com>
2016-07-03 19:48:33 -0700

    /spawn

--- NDN/src/com/mcs/ndn/Asshole.java
+++ NDN/src/com/mcs/ndn/Asshole.java
@@ -7,47 +7,72 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
-public class Asshole implements Listener{
-	
+public class Asshole implements Listener {
+	Random r = new Random();
+
 	@EventHandler
-	public void onPlayerChat(AsyncPlayerChatEvent event){
-//		if(event.getPlayer().getName().equals("AaronNerd07")){
-//			String[] quotes = {"...", "Such a good movie...", "IM BATMAN", "Hmm...what to do with $400?", "Subscribe to Universe Comics!", "Hello YouTube!", "Peace", "Im not gonna waste my shot!", "Im not stupid?", "Delete Channel ->[Yes]<-  [No]", "Im a diamond in the rough", "..."};
-//			Random r = new Random();
-//			int quote = r.nextInt(quotes.length);
-//			event.setMessage(quotes[quote]);
-//			
-//			Player player = (Player) event.getPlayer();
-//			
-//			if(quote == 6){
-//				//player.kickPlayer("Peace");
-//			}
-//			if(quote == 11){
-//				event.setMessage("DDoS me pls:" + player.getAddress());
-//			}
-	if(event.getPlayer().getName().equals("chmilk")){
-		Random r = new Random();
-		if(r.nextInt(50) > 47){
-		String message = event.getMessage();
-		event.setMessage(message + ", and by, " + message + " I mean HEY ASSHOLES!");
-		}
-	}
-//		}else if(event.getPlayer().getName().equals("matthew816")){
-//			String message = event.getMessage();
-//			if(Math.random() > 0.5f){
-//			event.setMessage(message + ", and I need a buy");
-//			}else{
-//				event.setMessage(message + ", remember 2-1-2, look at the minimap for godsake!");
-//			}
-//			}else if(event.getPlayer().getName().equals("Enderlord_jbs")){
-//				String message = event.getMessage();
-//				message += ", And matthew is the best brother in the world...";
-//				event.setMessage(message);
-//			}
+	public void onPlayerChat(AsyncPlayerChatEvent event) {
 		Player player = (Player) event.getPlayer();
+		if (r.nextInt(1000) == 0) {
+			String[] quotes = {"...", "sombody is typing for my account", "Vape Nation"};
+		}
+		 if(event.getPlayer().getName().equals("AaronNerd07")){
+		 String[] quotes = {"...", "Such a good movie...", "IM BATMAN",
+		 "Hmm...what to do with $400?", "Subscribe to Universe Comics!",
+		 "Hello YouTube!", "Peace", "Im not gonna waste my shot!", "Im not stupid?"
+		 , "Delete Channel ->[Yes]<- [No]", "Im a diamond in the rough", "...",
+		 "Bain, Bain is the source of the payday gang!", "Dammit james, you did not give me enough quotes",
+		 "sombody is typing for my account"};
+		 Random r = new Random();
+		 if (r.nextInt(200) == 0) {
+			 event.setMessage(quotes[r.nextInt(quotes.length)]);
+		}
+//		 event.setMessage(quotes[quote]);
+//		
+//		 Player player = (Player) event.getPlayer();
+//		
+//		 if(quote == 6){
+//		 //player.kickPlayer("Peace");
+//		 }
+//		 if(quote == 11){
+//		 event.setMessage("DDoS me pls:" + player.getAddress());
+		 }
+		 else if (event.getPlayer().getName().equals("chmilk")) {
+
+			if (r.nextInt(50) > 47) {
+				String message = event.getMessage();
+				event.setMessage(message + ", and by, " + message + " I mean HEY ASSHOLES!");
+			} else if (r.nextInt(50) > 47) {
+				String message = event.getMessage();
+				event.setMessage(message + ", and i worked on this plugin");
+			}
+		}
+		 else if (event.getPlayer().getName().equals("matthew816")) {
+			String message = event.getMessage();
+			if (r.nextInt(50) > 47) {
+				event.setMessage(message + ", and I need a buy");
+			} else if (r.nextInt(50) > 47) {
+				event.setMessage(message + ", remember 2-1-2, look at the minimap for godsake!");
+			}
+		}
+		 else if (event.getPlayer().getName().equals("Enderlord_jbs")) {
+			if (r.nextInt(50) > 47) {
+				String message = event.getMessage();
+				message += ", And matthew is the best brother in the world...";
+				event.setMessage(message);
+			}
 
 		}
-	
+		 else if (event.getPlayer().getName().equals("cupcake20009")) {
+			if (r.nextInt(500) > 69) {
+				String message = event.getMessage();
+				message += ", and i really like james";
+				event.setMessage(message);
+			}
+
+		}
+		
+
 	}
 
-
+}
--- NDN/src/com/mcs/ndn/NDN.java
+++ NDN/src/com/mcs/ndn/NDN.java
@@ -11,67 +11,85 @@ import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class NDN extends JavaPlugin{
+public class NDN extends JavaPlugin {
+	float afkx;
+	float afky;
+	float afkz;
+	Random r = new Random();
 
-	public void onEnable(){
+	public void onEnable() {
 		PluginDescriptionFile pdfFile = getDescription();
 		PluginManager pm = getServer().getPluginManager();
 		pm.registerEvents(new Asshole(), this);
 		Logger logger = getLogger();
 		logger.info(pdfFile.getName() + "Has been enabled!");
 	}
-	
-	public void onDisable(){
+
+	public void onDisable() {
 		PluginDescriptionFile pdfFile = getDescription();
 		Logger logger = getLogger();
 		logger.info(pdfFile.getName() + "Has been disabled!");
 	}
-	
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
+
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		Player player = (Player) sender;
 		Logger logger = getLogger();
 		logger.info("LABEL: " + label);
-		if(label.equals("sun")){
-			if(sender instanceof Player){
-				Player player = (Player) sender;
+		if (label.equals("sun")) {
+			if (sender instanceof Player) {
 				getServer().dispatchCommand(getServer().getConsoleSender(), "weather clear");
 				getServer().dispatchCommand(getServer().getConsoleSender(), "money take " + player.getName() + " 8");
 				getServer().dispatchCommand(getServer().getConsoleSender(), "money give city 8");
 				player.sendMessage(ChatColor.RED + "-$8");
 				player.sendMessage(ChatColor.GOLD + "Thank you for your donation, enjoy the nice weather!");
+			}
 		}
+		if (label.equals("day")) {
+			if (sender instanceof Player) {
+
+				getServer().dispatchCommand(getServer().getConsoleSender(), "time set 0");
+				getServer().dispatchCommand(getServer().getConsoleSender(), "money take " + player.getName() + " 20");
+				getServer().dispatchCommand(getServer().getConsoleSender(), "money give city 20");
+				player.sendMessage(ChatColor.RED + "-$20");
+				player.sendMessage(ChatColor.GOLD + "Thank you for your donation, time has been set to 0!");
+			}
+
 		}
-		if(label.equals("day")){
-						if(sender instanceof Player){
-							Player player = (Player) sender;
-							getServer().dispatchCommand(getServer().getConsoleSender(), "time set 0");
-							getServer().dispatchCommand(getServer().getConsoleSender(), "money take " + player.getName() + " 20");
-							getServer().dispatchCommand(getServer().getConsoleSender(), "money give city 20");
-							player.sendMessage(ChatColor.RED + "-$20");
-							player.sendMessage(ChatColor.GOLD + "Thank you for your donation, time has been set to 0!");
-						}
-				
+		if (label.equals("afk")) {
+
+			getServer().dispatchCommand(getServer().getConsoleSender(), "say " + player.getName() + " has gon AFK");
 		}
-		if(label.equals("lego")){
+
+		if (label.equals("spawn")) {
+
+			getServer().dispatchCommand(getServer().getConsoleSender(), "tp " + player.getName() + " 182, 70, 294");
+		}
+		if (label.equals("dev") && (player.getName().equals("chmilk") || player.getName().equals("matthew816"))) {
+
+			player.sendMessage(ChatColor.RED + "NDN is working");
+		}
+		if (label.equals("lego")) {
 			String mess = "Go home, LegoClone";
-			if(sender instanceof Player){
-				Player player = (Player) sender;
-				for(int i = 0; i < 10; i++){
+			if (sender instanceof Player) {
+
+				for (int i = 0; i < 10; i++) {
 					Random r = new Random();
-					int col = r.nextInt(5);
-					if(col == 0){
+					switch (r.nextInt(5)) {
+					case 0:
 						player.sendMessage(ChatColor.RED + mess);
-					}
-					if(col == 1){
+						break;
+					case 1:
 						player.sendMessage(ChatColor.YELLOW + mess);
-					}
-					if(col == 2){
+						break;
+					case 2:
 						player.sendMessage(ChatColor.BLUE + mess);
-					}
-					if(col == 3){
+						break;
+					case 3:
 						player.sendMessage(ChatColor.GREEN + mess);
-					}
-					if(col == 4){
+						break;
+					case 4:
 						player.sendMessage(ChatColor.WHITE + mess);
+						break;
 					}
 				}
 				getServer().dispatchCommand(getServer().getConsoleSender(), "kick legoclone09");
@@ -79,5 +97,5 @@ public class NDN extends JavaPlugin{
 		}
 		return false;
 	}
-	
+
 }
